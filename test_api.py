be5c5e779d2b8a6e07ee59f03c3256db56a06fc4
Otshepeng <219079149@student.uj.ac.za>
2026-03-06 12:28:16 +0200

    Added the ci unti Tests

---
+++ test_api.py
@@ -0,0 +1,16 @@
+import pytest
+import httpx
+from api_integration import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+def test_validate_true():
+    response = client.get("/validate/2222405343248877")
+    assert response.status_code == 200
+    assert response.json() == {"number": "2222405343248877", "valid": True}
+
+def test_validate_false():
+    response = client.get("/validate/2222405343248878")
+    assert response.status_code == 200
+    assert response.json() == {"number": "2222405343248878", "valid": False}
